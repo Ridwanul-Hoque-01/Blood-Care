b4720cf529c3a30e231eb553028dd6a29f8f612b
Ridwanul-Hoque-01 <ridwanulsagor123@gmail.com>
2025-11-12 21:18:04 +0600

    structuring Home page

---
+++ Home_Page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Care</title>
+</head>
+<body>
+    <div class="container">
+            <h2>Blood care Dashboard</h2>
+            <p>Your trusted platform for saving lives</p>
+            <h3>Select Your Portal</h3>
+            <form action="">
+                <button type="submit" class="btn" name="User">User</button>
+            </form>
+
+            <form action="">
+                <button type="submit" class="btn" name="Admin">Admin</button>
+            </form>
+            
+            <form action="">
+                <button type="submit" class="btn" name="Blood Bank">Blood Bank</button>
+            </form>
+    </div>
+</body>
+</html>
