1cc6b948effe599620e566af03f4b32ecb39a331
Ridwanul-Hoque-01 <ridwanulsagor123@gmail.com>
2025-11-13 12:56:04 +0600

    layout -login page

---
+++ User_Login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Care</title>
+</head>
+<body>
+    <header>Login </head>
+    <form method="post">
+        <div class="form-group">
+            <input type="text" calss="from-control" placeholder="User Id"utocomplete="off" required>
+        </div>
+
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password" name="Password" autocomplete="off" required>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn" name="submit">Submit</button>
+         </div>
+         
+        <div class="links">
+             Don't have an account? <a href="">Create Account</a>
+        </div>
+    </form>            
+    
+</body>
+</html>
