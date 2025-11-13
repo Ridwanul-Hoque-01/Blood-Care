05ae8339db526924d3441296c39f36b1a69c7d34
Ridwanul-Hoque-01 <ridwanulsagor123@gmail.com>
2025-11-13 21:26:09 +0600

    updating framework user dashboard

---
+++ User_dashboard.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Care</title>
+</head>
+<body>
+    <div class="header">
+        <div>
+            <h2>Hello</h2>
+            <p>ID:</p>
+        </div>
+        <form action="">
+            <button type="submit" class="btn">Logout</button>
+        </form>
+    </div>
+    <div class="container">
+        <div class="sidebar">
+            <form action="">test</form>
+            <form action="">test</form>
+            <form action="">test</form>
+
+        </div>
+        <div class="blog">
+            <h2>Blood Donation Blogs</h2>
+
+            <div class="blog-card">
+                <div class="blog-card-content">
+                    <h3>lalalala</h3>
+                    <p>dfdfdfd</p>
+                </div>
+            </div>
+
+            <div class="blog-card">
+                <div class="blog-card-content">
+                    <h3>lalalala</h3>
+                    <p>dfdfdfd</p>
+                </div>
+            </div>
+
+            <div class="blog-card">
+                <div class="blog-card-content">
+                    <h3>lalalala</h3>
+                    <p>dfdfdfd</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+    <footer>
+        <p>contact us:</p>
+    </footer>
+    
+</body>
+</html>
